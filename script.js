a75b9f9c6a84db81b44d291193e3bace2e054ad3
Jfrancis0518 <jfrancis0518@gmail.com>
2023-07-10 22:43:49 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-window.alert(W?)
+//var correct = 0
+//var incorrect = 0 
+var questions = [
+    {
+        prompt: "Which of this options control the styling for a webpage?\n(a) HTML\n\ (b) Photoshop\n\ (c) Google\n(d) CSS",
+        answer: "d" 
+    },
+    {
+        Prompt: "What do we use to allow CSS amd JavaScript to select and access specific elements?\n(a) Class\n\ (b) Src\n (c) alt\n (d) Semantic tags",
+        amswer: "a"
+    },
+    {
+        Prompt: "which of the following formats are used for highligting text? \n(a) sub\n\ (b) mark\n (c) del\n\ (d)em",
+        answer: "b"
+    },
+    {
+        prompt: "Which of the following should you add to your ReadMe?n\(a) project name\n\ (b) project description\n\ (c) what you learned\n (d) all of the above?",
+        answer: "d"
+    },
+    {
+        prompt: "What is A programming language originally created to be run in the browser with the intention of enhancing a webpage's capabilities?\n\(a) JavaScript\n\ (b) CSS\n\ (c) conditional statements\n (d)HTML",
+        answer: "a"
+    },
+]
+
+var startQuiz = function() {
+    //
+}
